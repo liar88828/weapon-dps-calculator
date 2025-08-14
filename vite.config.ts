a896a49937c062dccee031303140dd56c2d47af1
brian <liar88828@gmail.com>
2025-08-14 13:02:37 +0700

    finish

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: '/weapon-dps-calculator',
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
